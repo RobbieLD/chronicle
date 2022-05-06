023fb22439daa0124617ad34f22910d58b056f1d
Rob Davis <robd@youi.com>
2022-05-06 11:07:48 +1000

    Setting default background

--- src/store/modules/ui.module.ts
+++ src/store/modules/ui.module.ts
@@ -10,7 +10,7 @@ export default class UIModule implements Module<UIState, RootState> {
             addPanelOpen: false,
             settingsPanelOpen: false,
             title: '',
-            background: '',
+            background: 'https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80',
         }
     }
 
